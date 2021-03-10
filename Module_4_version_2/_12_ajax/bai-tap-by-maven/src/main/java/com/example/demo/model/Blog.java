01b93aa16582716c80e56d235fa2a8d524378cf7
KhangKunNguyen <phuckhang98@gmail.com>
2021-03-10 18:05:33 +0700

    khang commit

---
+++ Module_4_version_2/_12_ajax/bai-tap-by-maven/src/main/java/com/example/demo/model/Blog.java
@@ -0,0 +1,63 @@
+package com.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+public class Blog {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String title;
+    private String content;
+    private String date;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    @JsonBackReference
+    private Category category;
+
+    public Blog() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
